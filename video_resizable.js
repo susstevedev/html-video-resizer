3a16ea1710e21068891b053da65b90686f500771
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-09 10:16:32 -0500

    Add files via upload

--- video_resizable.js
+++ video_resizable.js
@@ -1,7 +1,16 @@
-function vid() {
-	document.getElementById("video").width = slider.value
-}
+function resize() {
+	document.getElementById("video").style.width = slider.value;
+	document.getElementById("video").style.height = "240px";
+};
 function fullscreen() {
-	document.getElementById("video").width = 100%
-	document.getElementById("video").height = 100%
+	document.getElementById("video").style.width = "100%";	document.getElementById("video").style.height = "100%";
+};
+let vid = document.getElementById("video");
+
+function play() {
+    vid.play();
+}
+
+function pause() {
+    vid.pause();
 }
