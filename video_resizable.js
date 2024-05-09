ab1166dcdd4922587e6ae36473d740196e744631
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-09 09:41:14 -0500

    Update video_resizable.js

--- video_resizable.js
+++ video_resizable.js
@@ -1,3 +1,7 @@
 function vid() {
 	document.getElementById("video").width = slider.value
 }
+function fullscreen() {
+	document.getElementById("video").width = 100%
+	document.getElementById("video").height = 100%
+}
